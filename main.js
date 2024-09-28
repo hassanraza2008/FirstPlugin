63603a6c91933be2643b572763a4033850a71f78
Hassan Raza <mrismart70@gmail.com>
2024-09-29 00:25:07 +0500

    appending 'Copy' btn to all elements

--- main.js
+++ main.js
@@ -2,4 +2,7 @@ var arr = document.getElementsByTagName("pre");
 
 for(let i = 0; i < arr.length; i++) {
     var btn = document.createElement("button");
+    btn.classList.add("copy_code_fcc_btn");
+    btn.appendChild(document.createTextNode("Copy"));
+    arr[i].appendChild(btn)
 }
